30ef31c7d90d56f10556ff2999b393c88f9f49e8
YanjunYu-1 <y9y9j8@163.com>
2022-05-20 20:06:39 -0500

    代码复用性小，要改为新的形式来写

--- StrategyPatten_Compressor_1/Program.cs
+++ StrategyPatten_Compressor_1/Program.cs
@@ -1,6 +1,8 @@
 ﻿// create a WinRAR clone创建一个WinRAR克隆
 //WINRAR can compress and decompress ZIP or RAR filesWINRAR可以压缩和解压ZIP或RAR文件
 
+//以此形式要建立多个类，代码复用性小，要改为新的形式来写
+
 public interface Compressor//压缩
 {
     void Compress();
@@ -8,7 +10,9 @@ public interface Compressor//压缩
 
 public class RARCompress : Compressor
 {
-    public int FileSize;
+    public int FileSize;//文件大小
+    public enum BinarySize { };//32 bit or 64 bit?
+    public string EncodingType;//编码类型
     public void Compress()
     {
         //compress to rar
@@ -17,6 +21,7 @@ public class RARCompress : Compressor
 
 public class ZipCompress : Compressor
 {
+    public string Metadata { get; set; }//元数据
     public void Compress()
     {
         //compress to rar
