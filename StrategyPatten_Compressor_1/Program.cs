cc2f264559b3fe13d3256b6904eff94fac0ee670
YanjunYu-1 <y9y9j8@163.com>
2022-05-20 19:55:55 -0500

    搞明白了  public void SetFlyBehaviour(FlyBehaviour fb)     {         FlyBehaviour = fb;     }     public void SetQuackBehaviour(QuackBehaviour qb)     {         QuackBehaviour = qb;     }

---
+++ StrategyPatten_Compressor_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// create a WinRAR clone创建一个WinRAR克隆
+//WINRAR can compress and decompress ZIP or RAR filesWINRAR可以压缩和解压ZIP或RAR文件
+
+public interface Compressor//压缩
+{
+    void Compress();
+}
+
+public class RARCompress : Compressor
+{
+    public int FileSize;
+    public void Compress()
+    {
+        //compress to rar
+    }
+}
+
+public class ZipCompress : Compressor
+{
+    public void Compress()
+    {
+        //compress to rar
+    }
+}
+
+public class Client//客户
+{
+
+}
