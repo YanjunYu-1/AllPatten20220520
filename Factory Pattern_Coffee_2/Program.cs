33b9192cd469990c2179abfdd4fab6906eebe27d
YanjunYu-1 <y9y9j8@163.com>
2022-05-23 18:52:31 -0500

    finish Lab1

--- Factory Pattern_Coffee_2/Program.cs
+++ Factory Pattern_Coffee_2/Program.cs
@@ -2,22 +2,70 @@
 
 //如果我们期望一个类的一些东西改变，而一些东西保持不变，一些类可以重用其他类的行为，但不是全部，我们应该使用策略模式
 
-
 // code against interfaces代码对接口
+
+BeverageVendor dirveThru=new DriveThruVendor();
+dirveThru.Order("coffee");
+BeverageVendor MoonDollars=new FancyVendor();
+MoonDollars.Order("coffee");
 public abstract class BeverageVendor//饮料供应商
 {
     // beverage vendor ALWAYS runs the factory method and then the Pour, Lid, Sell methods饮料供应商总是先运行factory方法，然后运行Pour, Lid, Sell方法
-    BeverageFactory factory;
 
-    public Beverage MakeBeverage(string order)
+    public Beverage Order(string order)
     {
         Beverage beverage;
-        beverage = factory.CreateBeverage(order);
+        beverage = createBeverage(order);
         beverage.Pour();
         beverage.Lid();
         beverage.Sell();
 
         return beverage;
+    }
+    public abstract Beverage createBeverage(string order);
+}
+
+public class DriveThruVendor : BeverageVendor
+{
+    //only sell coffee and tea
+    public override Beverage createBeverage(string type)
+    {
+        Beverage beverage;
+        if (type == "tea")
+        {
+            beverage = new Tea();
+            return beverage;
+        }else if (type == "coffee")
+        {
+            beverage = new Coffee();
+            return beverage;
+        }else
+        {
+            throw new Exception("We don't serve that here, you maniac");//我们这里不卖这个，你个疯子
+        }
+    }
+}
+
+public class FancyVendor : BeverageVendor
+{
+    //only sell coffee and tea
+    public override Beverage createBeverage(string type)
+    {
+        Beverage beverage;
+        if (type == "coffee")
+        {
+            beverage = new Espresso();
+            return beverage;
+        }
+        else if (type == "tea")
+        {
+            beverage = new Tea();
+            return beverage;
+        }
+        else
+        {
+            throw new Exception("We don't serve that here, you maniac");//我们这里不卖这个，你个疯子
+        }
     }
 }
 public abstract class Beverage
---
+++ Lab1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Configuration;
+
+
+
+var TrainTicket = new TrainTicket();
+
+
+Console.WriteLine("Which month's train tickets do you want to check?");
+string monthStr=Console.ReadLine();
+int monthInt=Convert.ToInt32(monthStr);
+
+TrainTicket.Fee(monthInt);
+
+
+//while (monthInt < 1 || monthInt > 12)
+//{
+//    Console.WriteLine("请输入六位数学号");
+    
+//}
+
+//Convert.ToInt32(Console.ReadLine());
+
+
+
+
+public class TrainTicket
+{
+    string path = @"G:\3.MITT\软件工程和设计模式-SD-350-F21P1\AllPatten20220520\Lab1\GetResult.txt";
+    public StrategyPatternTicket StrategyPatternTicket { get; set; }
+    int payMoney = 50;
+
+    public double Fee(int month)
+    {
+        string saveRecordLine = "";
+        if (month == 12)
+        {
+            StrategyPatternTicket = new December();
+            saveRecordLine = $"{payMoney} {month} 0 {StrategyPatternTicket.Fee()}";
+        }
+        else if (month == 6 || month == 7)
+        {
+            StrategyPatternTicket = new JuneOrJuly();
+            saveRecordLine = $"{payMoney} {month} 25 {StrategyPatternTicket.Fee()}";
+
+        }
+        else
+        {
+            StrategyPatternTicket = new Usual();
+            saveRecordLine = $"{payMoney} {month} 0 {StrategyPatternTicket.Fee()}";
+
+        }
+
+        Console.WriteLine(saveRecordLine);
+
+        try
+        {
+            StreamWriter writerToPath = new StreamWriter(path, true);
+            writerToPath.WriteLine(saveRecordLine);
+            writerToPath.Close();
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+
+        return StrategyPatternTicket.Fee();
+    }
+
+
+}
+
+public interface StrategyPatternTicket
+{
+    public double Fee();
+}
+
+public class Usual : StrategyPatternTicket
+{
+    int payMoney = 50;
+
+    public double Fee()
+    {
+
+        return Convert.ToDouble(payMoney);
+    }
+}
+
+public class JuneOrJuly : StrategyPatternTicket
+{
+    int payMoney = 50;
+    public double Fee()
+    {
+        return Convert.ToDouble(payMoney) * 0.75;
+    }
+}
+
+public class December : StrategyPatternTicket
+{
+    int payMoney = 50;
+    public double Fee()
+    {
+        return Convert.ToDouble(payMoney) * 2;
+    }
+}
